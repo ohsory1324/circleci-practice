3991d559477a0045d9212ba3c83dc01ad8562a65
ohsory1324 <ohsory1324@naver.com>
2017-05-26 00:13:07 +0900

    test

---
+++ dbUpdate.js
@@ -0,0 +1,13 @@
+var knex = require("knex")({
+  client: "mysql",
+  connection: {
+    host: process.env["AWS_RDS_HOST_URL"],
+    user: process.env["AWS_RDS_USER"],
+    password: process.env["AWS_RDS_PASSWORD"],
+    database: process.env["AWS_RDS_DATABASE"],
+  },
+});
+
+knex('customer').insert({
+  github: process.env["CIRCLE_USERNAME"],
+});

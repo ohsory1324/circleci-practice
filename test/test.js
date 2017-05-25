124a8c24895af3b7fcead4dbabf92fcf793a95ef
ohsory1324 <ohsory1324@naver.com>
2017-05-25 23:57:07 +0900

    init

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var chai = require('chai');
 var expect = chai.expect;
 
 describe('Test', function() {
-  it('log username', function() {
+  it('log project username', function() {
     console.log(process.env["CIRCLE_PROJECT_USERNAME"]);
   });
 

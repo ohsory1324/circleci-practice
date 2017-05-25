be6ccceb1a0764e24df67a361de55e2c6f48618f
ohsory1324 <ohsory1324@naver.com>
2017-05-25 20:18:05 +0900

    test env var

--- test/test.js
+++ test/test.js
@@ -2,7 +2,15 @@ var chai = require('chai');
 var expect = chai.expect;
 
 describe('Test', function() {
-  it('typeof 1 + 1 is a number', function() {
+  it('log username', function() {
+    console.log(process.env["CIRCLE_PROJECT_USERNAME"]);
+  });
+
+  it('log reponame', function() {
+    console.log(process.env["CIRCLE_PROJECT_REPONAME"]);
+  });
+
+  it('type of 1 + 1 is a number', function() {
     expect(1 + 1).to.be.a('number');
   });
 
